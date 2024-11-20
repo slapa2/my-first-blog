62ae9646371366f3af52716c6617cd576dc1eefa
slapa2 <slawomirpawlowski@gmail.com>
2024-11-20 22:11:37 +0100

    added draft and delete post

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -46,3 +45,23 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     context = {'form': form}
     return render(request, 'blog/post_edit.html', context)
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    context = {'posts': posts}
+    return render(request, 'blog/post_draft_list.html', context)
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method=='POST':
+        post.delete()
+    return redirect('post_list')
